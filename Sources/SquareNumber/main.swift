3845ffe513b608e83ade7964db5971c557db32d5
Ivan C Myrvold <ivan@myrvold.org>
2020-12-29 13:08:24 +0100

    added a debug print to lambda function

--- Sources/SquareNumber/main.swift
+++ Sources/SquareNumber/main.swift
@@ -9,6 +9,7 @@ struct Output: Codable {
 }
 
 Lambda.run { (context, input: Input, callback: @escaping (Result<Output, Error>) -> Void) in
+    print("input:", input)
     callback(.success(Output(result: input.number * input.number)))
 }
 
